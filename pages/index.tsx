43135bf1e724cc446c72a4ef53a8bc0a34ae8462
Giulio Rossi <ciuffi9@mac.com>
2020-01-26 04:08:24 -0800

    Started adding RunHistory and the rest of the frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import { NextPage } from "next";
 import Layout from "../components/Layout";
-const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-    <Layout>
-        <h1>Hello world! - user agent: {userAgent}</h1>
-    </Layout>
+import RunHistory from "../components/RunHistory";
+const Home: NextPage = () => (
+  <Layout>
+    <section className="hero is-info">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">BRC AutoRun</h1>
+          <h2 className="subtitle">Management System</h2>
+        </div>
+        <RunHistory />
+      </div>
+    </section>
+  </Layout>
 );
 
 Home.getInitialProps = async ({ req }) => {
-    const userAgent = req
-        ? req.headers["user-agent"] || ""
-        : navigator.userAgent;
-    return { userAgent };
+  const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
+  return { userAgent };
 };
 
 export default Home;
