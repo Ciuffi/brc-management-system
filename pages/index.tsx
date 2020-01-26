86b2cff40d4a539289d5817fa1dbde2d2db833a1
Giulio Rossi <ciuffi9@mac.com>
2020-01-26 02:41:38 -0800

    Added bulma to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
-import { NextPage } from 'next';
-
+import { NextPage } from "next";
+import Layout from "../components/Layout";
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <h1>Hello world! - user agent: {userAgent}</h1>
+    <Layout>
+        <h1>Hello world! - user agent: {userAgent}</h1>
+    </Layout>
 );
 
 Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
-  return { userAgent };
+    const userAgent = req
+        ? req.headers["user-agent"] || ""
+        : navigator.userAgent;
+    return { userAgent };
 };
 
 export default Home;
