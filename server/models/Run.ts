6cac6e3c8e9c2d0bc018fb40a04a3e22259d9271
Giulio Rossi <ciuffi9@mac.com>
2020-01-26 04:08:01 -0800

    Updated Run schema

--- server/models/Run.ts
+++ server/models/Run.ts
@@ -1,19 +1,21 @@
 interface Run {
-    //Identifier information
-    LabName: string;
-    RunName: string;
-    //Pipeline stage information
-    RunFinished: boolean;
-    AnalysisFinished?: boolean;
-    Error: boolean;
-    //stats information
-    CreatedTime: string;
-    RunFinishedTime?: string;
-    AnalysisFinishedTime?: string;
-    //File locations
-    BCLFolderPath?: string;
-    BCLFilePaths?: string[];
-    FASTQFolderPath?: string;
-    FASTQFilePaths?: string[];
+  //Identifier information
+  LabName: string;
+  RunName: string;
+  _id?: number;
+  //Pipeline stage information
+  RunFinished: boolean;
+  AnalysisFinished?: boolean;
+  Error: boolean;
+  //stats information
+  CreatedTime: string;
+  AnalysisStartTime: string;
+  RunFinishedTime?: string;
+  AnalysisFinishedTime?: string;
+  //File locations
+  BCLFolderPath?: string;
+  BCLFilePaths?: string[];
+  FASTQFolderPath?: string;
+  FASTQFilePaths?: string[];
 }
 export default Run;
