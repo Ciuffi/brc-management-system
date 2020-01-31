06d4f81bd0cb6744671f5d359e0d426cba63061c
Giulio Rossi <ciuffi9@mac.com>
2020-01-31 12:50:54 -0800

    fix api base path issues

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const production = process.env.NODE_ENV === "production";
 module.exports = {
   ...withSass(),
   assetPrefix: production ? "/bms" : "",
-  publicRuntimeConfig: {
+  experimental: {
     basePath: production ? "/bms" : ""
   }
 };
