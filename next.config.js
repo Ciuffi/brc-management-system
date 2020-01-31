539675c935c1f4c98ed252a515d708f15255fd1b
Giulio Rossi <ciuffi9@mac.com>
2020-01-31 02:05:48 -0800

    setup base path for bms

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const withSass = require("@zeit/next-sass");
+const production = process.env.NODE_ENV === "production";
 module.exports = {
-    ...withSass()
+  ...withSass(),
+  assetPrefix: production ? "/bms" : "",
+  publicRuntimeConfig: {
+    basePath: production ? "/bms" : ""
+  }
 };
