7b862549874b7a9a895561733b7a1cc7cc476d2a
Giulio Rossi <ciuffi9@mac.com>
2020-02-06 15:59:31 -0800

    serve bcl files

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import next from "next";
+import path from "path";
+import directory from "serve-index";
 import DBHandler from "./DBHandler";
 import Watcher from "./Watcher";
 const port = parseInt(process.env.PORT || "", 10) || 3000;
@@ -7,12 +9,16 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const dbhandler = new DBHandler();
+const bclFilesPath = "/brcwork/sequence/bcl/";
+console.log(bclFilesPath);
 app
   .prepare()
   .then(dbhandler.initialize)
   .then(() => Watcher(dbhandler))
   .then(() => {
     const server = express();
+
+    server.use("/bcl", express.static(bclFilesPath), directory(bclFilesPath));
 
     server.use((req, res, next) => {
       (req as any).db = dbhandler;
