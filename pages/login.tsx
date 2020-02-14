4e2bdb249a863067caafd2c01f31e3a5643ace37
Giulio Rossi <ciuffi9@mac.com>
2020-02-13 17:11:19 -0800

    fix background styling

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,38 +9,40 @@ interface loginProps {
 }
 
 const Login = ({ basePath, error }: loginProps) => (
-  <Layout>
-    <section className="hero is-info">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">BRC AutoRun</h1>
-          <h2 className="subtitle">
-            <div> Management System</div>
-          </h2>
+  <div style={{ backgroundColor: "lightgrey" }}>
+    <Layout>
+      <section className="hero is-info">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">BRC AutoRun</h1>
+            <h2 className="subtitle">
+              <div> Management System</div>
+            </h2>
+          </div>
         </div>
+      </section>
+      <div id="mainDiv">
+        <article id="loginCard" className="message">
+          <div className="message-header">
+            <p>Login</p>
+          </div>
+          <div className="message-body">
+            <div className="intro">
+              Please login with your lab's shared password. <br />
+            </div>
+            <LoginForm basePath={basePath} error={error} />
+            <div className="intro" style={{ marginTop: "2%" }}>
+              If you have any issues logging in, please contact Giulio Rossi @{" "}
+              <a href="mailto:ciuffi9@mac.com?subject=BMS support">
+                ciuffi9@mac.com
+              </a>{" "}
+              for assistance.
+            </div>
+          </div>
+        </article>
       </div>
-    </section>
-    <div id="mainDiv">
-      <article id="loginCard" className="message">
-        <div className="message-header">
-          <p>Login</p>
-        </div>
-        <div className="message-body">
-          <div className="intro">
-            Please login with your lab's shared password. <br />
-          </div>
-          <LoginForm basePath={basePath} error={error} />
-          <div className="intro" style={{ marginTop: "2%" }}>
-            If you have any issues logging in, please contact Giulio Rossi @{" "}
-            <a href="mailto:ciuffi9@mac.com?subject=BMS support">
-              ciuffi9@mac.com
-            </a>{" "}
-            for assistance.
-          </div>
-        </div>
-      </article>
-    </div>
-  </Layout>
+    </Layout>
+  </div>
 );
 
 Login.getInitialProps = (context: NextPageContext) => {
