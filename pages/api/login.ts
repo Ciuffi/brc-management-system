7668efdc9574efede157cac04f67e5d70ab81acb
Giulio Rossi <ciuffi9@mac.com>
2020-02-13 16:37:28 -0800

    remove uselss api endpoint

--- pages/api/login.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { pass } = JSON.parse(req.body);
-  if (!pass) {
-  }
-};
