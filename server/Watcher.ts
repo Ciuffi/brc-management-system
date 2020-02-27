34db895d453a8099909b6acb3a2254f81021412f
Ciuffi <ciuffi9@mac.com>
2020-02-26 20:44:40 -0800

    minor code fix

--- server/Watcher.ts
+++ server/Watcher.ts
@@ -58,7 +58,7 @@ export default async (dbHandler: dbHandler): Promise<void> => {
     }
     await dbHandler.setWatcherStatus(true);
     greenLog("watcher online!");
-    console.log(watcher.getWatched());
+    console.log(watched);
     return;
   });
   watcher.on("add", onChange);
