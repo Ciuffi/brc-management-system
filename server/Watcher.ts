e1bf93c34035ff9fd3623711a1be38ebbf594d7c
Ciuffi <ciuffi9@mac.com>
2020-02-26 21:28:19 -0800

    setup BCL Folder handling and better error logging

--- server/Watcher.ts
+++ server/Watcher.ts
@@ -22,46 +22,63 @@ export const greenLog = (str: string) => console.log(greenBright(str));
 export const yellowLog = (str: string) => console.log(yellowBright(str));
 
 export default async (dbHandler: dbHandler): Promise<void> => {
-  console.log("Starting watcher..");
+  await new Promise((resolve, reject) => {
+    let RTAFound = false;
+    console.log("> Starting watcher...");
 
-  const watcher = chokidar.watch(toWatch, {
-    persistent: true,
-    ignoreInitial: true,
-    ignored: ["*/.DS_Store"],
-    depth: 1
-  });
-
-  const handleRTAComplete = async (path: string) => {
-    const id = (await dbHandler.GetLatestRun())._id;
-    const BCLFolderPath = path.substring(0, path.lastIndexOf("/") + 1);
-    await dbHandler.updateRun(id, {
-      RunFinished: true,
-      RunFinishedTime: new Date().toISOString(),
-      BCLFolderPath
+    const watcher = chokidar.watch("/brcwork/sequence/bcl/**/*", {
+      persistent: true,
+      ignoreInitial: true,
+      ignored: ["*/.DS_Store"],
+      depth: 1
     });
-    greenLog(`RTAComplete found`);
-  };
 
-  const onChange = (path: string, stats?: Stats) => {
-    console.log(`path: ${path}\nstats: ${stats}`);
-    if (path.endsWith(RTAComplete)) {
-      handleRTAComplete(path);
-    }
-  };
+    watcher.on("ready", async () => {
+      const watched = watcher.getWatched();
+      if (Object.keys(watched).length === 0) {
+        console.error(
+          "> Unable to find folder to watch. Please make sure these folders exist: ",
+          toWatch
+        );
+        process.exit(1);
+      }
+      await dbHandler.setWatcherStatus(true);
+      greenLog("> watcher online!\n> Watching:");
+      console.log(watched);
+      resolve();
+    });
 
-  watcher.on("ready", async () => {
-    const watched = watcher.getWatched();
-    if (Object.keys(watched).length === 0) {
-      console.error(
-        "Unable to find folder to watch. Please make sure these folders exist: ",
-        toWatch
-      );
-      process.exit();
-    }
-    await dbHandler.setWatcherStatus(true);
-    greenLog("watcher online!");
-    console.log(watched);
-    return;
+    const handleRTAComplete = async (path: string) => {
+      const id = (await dbHandler.GetLatestRun())._id;
+      await dbHandler.updateRun(id, {
+        RunFinished: true,
+        RunFinishedTime: new Date().toISOString()
+      });
+      RTAFound = true;
+      greenLog(`> RTAComplete found`);
+      yellowLog("> Waiting for bcl folder..");
+    };
+
+    const handleBCLFolder = async (path: string) => {
+      const id = (await dbHandler.GetLatestRun())._id;
+      await dbHandler.updateRun(id, {
+        BCLFolderPath: path
+      });
+      greenLog("> BCL Folder created and associated.");
+      RTAFound = false;
+    };
+
+    const onChange = (path: string, stats?: Stats) => {
+      if (path.endsWith(RTAComplete)) {
+        handleRTAComplete(path);
+      } else if (RTAFound && stats?.isDirectory()) {
+        handleBCLFolder(path);
+      } else {
+        yellowLog("> File found bye listener but not processed.");
+      }
+    };
+
+    watcher.on("add", onChange);
+    watcher.on("addDir", onChange);
   });
-  watcher.on("add", onChange);
 };
