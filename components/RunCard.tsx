697d66b5666649fe2c0c0a1e3b0bf4065dfd0c04
Giulio Rossi <ciuffi9@mac.com>
2020-04-16 16:23:14 -0700

    Ability to show/hide runs

--- components/RunCard.tsx
+++ components/RunCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Run from "../server/models/Run";
 interface RunCardProps {
   run: Run;
@@ -14,53 +14,66 @@ const RunCard = ({
     BCLFilePaths,
     BCLFolderPath
   }
-}: RunCardProps) => (
-  <div
-    className={`message ${RunFinished ? "is-success" : ""} ${
-      !RunFinished && !Error ? "is-warning" : ""
-    } ${Error ? "is-danger" : ""}`}
-  >
-    <div className="message-header">
-      Name: {RunName}
-      {!Error ? <p>processed: {RunFinished ? "yes" : "no"}</p> : <p> Error!</p>}
+}: RunCardProps) => {
+  const [CardShown, SetCardShown] = useState(false);
+  return (
+    <div
+      className={`message ${RunFinished ? "is-success" : ""} ${
+        !RunFinished && !Error ? "is-warning" : ""
+      } ${Error ? "is-danger" : ""}`}
+    >
+      <div onClick={() => SetCardShown(!CardShown)} className="message-header">
+        Name: {RunName}{" "}
+        <span style={{ textAlign: "left", color: "blue", fontSize: "12px" }}>
+          {CardShown ? "Click to Hide Details" : "Click to Show Details"}
+        </span>
+        {!Error ? (
+          <p>processed: {RunFinished ? "yes" : "no"}</p>
+        ) : (
+          <p> Error!</p>
+        )}
+      </div>
+      <div
+        style={{ display: CardShown ? "inherit" : "none", textAlign: "left" }}
+        className="message-body"
+      >
+        <p>
+          <b>Run started on:</b> {new Date(CreatedTime).toLocaleString("en-US")}
+        </p>
+        <p>
+          <b>Run finished at: </b>{" "}
+          {RunFinishedTime
+            ? new Date(RunFinishedTime).toLocaleString("en-US")
+            : "unfinished"}
+        </p>
+        {BCLFolderPath ? (
+          <div>
+            <b>BCL folder path:</b>{" "}
+            <a
+              href={`/bms/bms/bcl/${BCLFolderPath.substr(
+                "/brcwork/sequence/bcl/".length
+              )}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {BCLFolderPath}
+            </a>
+          </div>
+        ) : null}
+        <p>
+          <b>Pipeline started on: </b>{" "}
+          {AnalysisStartTime
+            ? new Date(AnalysisStartTime).toLocaleString("en-US")
+            : "unfinished"}
+        </p>
+      </div>
+      <style jsx>{`
+        .bclPath {
+          margin-left: 2%;
+        }
+      `}</style>
     </div>
-    <div style={{ textAlign: "left" }} className="message-body">
-      <p>
-        <b>Run started on:</b> {new Date(CreatedTime).toLocaleString("en-US")}
-      </p>
-      <p>
-        <b>Run finished at: </b>{" "}
-        {RunFinishedTime
-          ? new Date(RunFinishedTime).toLocaleString("en-US")
-          : "unfinished"}
-      </p>
-      {BCLFolderPath ? (
-        <div>
-          <b>BCL folder path:</b>{" "}
-          <a
-            href={`/bms/bms/bcl/${BCLFolderPath.substr(
-              "/brcwork/sequence/bcl/".length
-            )}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {BCLFolderPath}
-          </a>
-        </div>
-      ) : null}
-      <p>
-        <b>Pipeline started on: </b>{" "}
-        {AnalysisStartTime
-          ? new Date(AnalysisStartTime).toLocaleString("en-US")
-          : "unfinished"}
-      </p>
-    </div>
-    <style jsx>{`
-      .bclPath {
-        margin-left: 2%;
-      }
-    `}</style>
-  </div>
-);
+  );
+};
 
 export default RunCard;
