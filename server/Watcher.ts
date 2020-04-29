c6a26fadea00e9c2079860ca141db6d56e90bc3f
Giulio Rossi <ciuffi9@mac.com>
2020-04-29 10:37:01 -0700

    remove RTAComplete code and add bcl watcher code

--- server/Watcher.ts
+++ server/Watcher.ts
@@ -18,7 +18,6 @@ if (!folderExists) {
 
 export default async (dbHandler: DbHandler): Promise<void> => {
   await new Promise((resolve, reject) => {
-    let RTAFound = false;
     console.log("> Starting watcher...");
 
     const watcher = chokidar.watch(`${folderPath}*`, {
@@ -36,47 +35,52 @@ export default async (dbHandler: DbHandler): Promise<void> => {
       resolve();
     });
 
-    const handleRTAComplete = async (path: string) => {
-      const lastRun = await dbHandler.GetLatestRun();
-      if (!lastRun) {
-        redLog("> No runs inputted but RTAComplete found.");
-        return;
-      }
-      if (lastRun.RunFinished) {
-        redLog(
-          `> New RTAComplete received but last run: ${lastRun.RunName} has already been processed.\nThis RTAComplete will be ignored.`
-        );
-        return;
-      }
-      const { _id: id } = lastRun;
-      await dbHandler.updateRun(id, {
-        RunFinished: true,
-        RunFinishedTime: new Date().toISOString()
-      });
-      RTAFound = true;
-      greenLog(`> RTAComplete found`);
-      yellowLog("> Waiting for bcl folder..");
-    };
+    // const handleRTAComplete = async (path: string) => {
+    //   const lastRun = await dbHandler.GetLatestRun();
+    //   if (!lastRun) {
+    //     redLog("> No runs inputted but RTAComplete found.");
+    //     return;
+    //   }
+    //   if (lastRun.RunFinished) {
+    //     redLog(
+    //       `> New RTAComplete received but last run: ${lastRun.RunName} has already been processed.\nThis RTAComplete will be ignored.`
+    //     );
+    //     return;
+    //   }
+    //   const { _id: id } = lastRun;
+    //   await dbHandler.updateRun(id, {
+    //     RunFinished: true,
+    //     RunFinishedTime: new Date().toISOString()
+    //   });
+    //   RTAFound = true;
+    //   greenLog(`> RTAComplete found`);
+    //   yellowLog("> Waiting for bcl folder..");
+    // };
 
     const handleBCLFolder = async (path: string) => {
-      const lastRun = await dbHandler.GetLatestRun();
-      if (!lastRun.RunFinished) {
-        redLog(
-          `> New folder created but last run: ${lastRun.RunName} has not been processed yet.\nThis folder will be ignored.`
-        );
+      const arr = path.split("/");
+      const folderName = arr.pop();
+      const runName = `${folderName
+        .split("_")
+        .slice(0, -1)
+        .join("_")}_XXXXXXXXXX`;
+      const lastRun = await dbHandler.GetRun(runName);
+      if (!lastRun) {
+        redLog(`> Run ${runName} not found.\nThis folder will be ignored.`);
+        return;
       }
       const { _id: id } = lastRun;
       await dbHandler.updateRun(id, {
-        BCLFolderPath: path
+        BCLFolderPath: path,
+        RunName: folderName,
+        RunFinished: true
       });
       greenLog("> BCL Folder created and associated.");
-      RTAFound = false;
+      //RTAFound = false;
     };
 
     const onChange = (path: string, stats?: Stats) => {
-      if (path.endsWith(RTAComplete)) {
-        handleRTAComplete(path);
-      } else if (RTAFound && stats?.isDirectory()) {
+      if (stats?.isDirectory()) {
         handleBCLFolder(path);
       } else {
         yellowLog("> File found by watcher but not processed.");
